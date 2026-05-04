02a7a8566c9b4e59180c1940a8c98bf731dce098
Mihai Secosan <secosanmihaisebastian@gmail.com>
2026-05-04 16:33:50 +0300

    Better ToolLLM

--- server/python/src/Agents/Brain/ToolLLM.py
+++ server/python/src/Agents/Brain/ToolLLM.py
@@ -1,18 +1,18 @@
 from langchain_core.messages import (
+    AIMessage as LangchainAIMessage,
     BaseMessage as LangchainBaseMessage,
-    SystemMessage as LangchainSystemMessage,
     HumanMessage as LangchainHumanMessage,
-    ToolMessage as LangchainToolMessage,
-    AIMessage as LangchainAIMessage
+    SystemMessage as LangchainSystemMessage,
+    ToolMessage as LangchainToolMessage
 )
 from langchain_core.messages.tool import ToolCall as LangchainToolCall
 from langchain_core.tools import BaseTool, StructuredTool
 from langchain_openai.chat_models.base import ChatOpenAI
 from pydantic import BaseModel, create_model
-from typing import Any
+from typing import Any, Callable
 
 from src.Agents.Tools.Tool import Tool
-from src.Agents.Messages.Messages import Message, SystemMessage, UserMessage, ToolCall, ToolMessage, AIMessage
+from src.Agents.Messages.Messages import AIMessage, Message, SystemMessage, ToolCall, ToolMessage, UserMessage
 
 
 class ToolLLM:
@@ -33,8 +33,8 @@ class ToolLLM:
 
         @staticmethod
         def parse_tool_message(tool_message: ToolMessage) -> LangchainToolMessage:
-            content = tool_message.get_content()
-            tool_call_id = tool_message.get_id()
+            content: str = tool_message.get_content()
+            tool_call_id: str = tool_message.get_id()
             return LangchainToolMessage(content=content, tool_call_id=tool_call_id)
 
         @staticmethod
@@ -45,20 +45,24 @@ class ToolLLM:
                 'id': tool_call.get_id()
             }
 
-        def parse_ai_message(self, ai_message: AIMessage) -> LangchainAIMessage:
-            content = ai_message.get_content()
-            tool_calls = [self.parse_tool_call(tc) for tc in ai_message.get_tool_calls()]
+        @staticmethod
+        def parse_ai_message(ai_message: AIMessage) -> LangchainAIMessage:
+            content: str = ai_message.get_content()
+            tool_calls: list[LangchainToolCall] = [
+                ToolLLM.MyClassesToLangchainParser.parse_tool_call(tool_call)
+                for tool_call in ai_message.get_tool_calls()
+            ]
             return LangchainAIMessage(content=content, tool_calls=tool_calls)
 
+        @staticmethod
         def parse_message(
-                self,
                 message: Message | SystemMessage | UserMessage | ToolMessage | AIMessage
         ) -> LangchainBaseMessage | LangchainSystemMessage | LangchainHumanMessage | LangchainToolMessage | LangchainAIMessage:
-            transformers = {
-                SystemMessage: self.parse_system_message,
-                UserMessage: self.parse_user_message,
-                ToolMessage: self.parse_tool_message,
-                AIMessage: self.parse_ai_message
+            transformers: dict[type, Callable] = {
+                SystemMessage: ToolLLM.MyClassesToLangchainParser.parse_system_message,
+                UserMessage: ToolLLM.MyClassesToLangchainParser.parse_user_message,
+                ToolMessage: ToolLLM.MyClassesToLangchainParser.parse_tool_message,
+                AIMessage: ToolLLM.MyClassesToLangchainParser.parse_ai_message
             }
             try:
                 return transformers[type(message)](message)
@@ -89,42 +93,43 @@ class ToolLLM:
 
         @staticmethod
         def parse_tool_call(langchain_tool_call: LangchainToolCall) -> ToolCall:
-            tool_name = langchain_tool_call.get('name')
-            parameters = langchain_tool_call.get('args')
-            tool_call_id = langchain_tool_call.get('id')
+            tool_name: str = langchain_tool_call.get('name')
+            parameters: dict[str, Any] = langchain_tool_call.get('args')
+            tool_call_id: str = langchain_tool_call.get('id')
 
             return ToolCall(tool_name, parameters, tool_call_id)
 
         @staticmethod
-        def parse_tool_message(langchain_tool_message: LangchainToolMessage) -> ToolMessage:
-            content = langchain_tool_message.content
-            tool_call_id = langchain_tool_message.tool_call_id
-            return ToolMessage(content, tool_call_id)
-
-        def parse_ai_message(self, langchain_ai_message: LangchainAIMessage) -> AIMessage:
-            content = langchain_ai_message.content
-            tool_calls = [self.parse_tool_call(tool_call) for tool_call in langchain_ai_message.tool_calls]
+        def parse_ai_message(langchain_ai_message: LangchainAIMessage) -> AIMessage:
+            content: str = langchain_ai_message.content
+            tool_calls: list[ToolCall] = [
+                ToolLLM.LangchainToMyClassesParser.parse_tool_call(tool_call)
+                for tool_call in langchain_ai_message.tool_calls
+            ]
             return AIMessage(content, tool_calls)
 
     def __init__(self, llm: ChatOpenAI, mine_to_lang_parser: MyClassesToLangchainParser,
                  lang_to_mine_parser: LangchainToMyClassesParser):
-        self.llm = llm
+        self.llm: ChatOpenAI = llm
         self.tool_llm = None
         self.tools: dict[str, Tool] = {}
-        self.mine_to_lang_parser = mine_to_lang_parser
-        self.lang_to_mine_parser = lang_to_mine_parser
+        self.mine_to_lang_parser: ToolLLM.MyClassesToLangchainParser = mine_to_lang_parser
+        self.lang_to_mine_parser: ToolLLM.LangchainToMyClassesParser = lang_to_mine_parser
 
-    def bind_tools(self, tools: dict[str, Tool]):
+    def bind_tools(self, tools: dict[str, Tool]) -> None:
         if not tools:
             self.tool_llm = None
-            self.tools: dict[str, Tool] = {}
+            self.tools.clear()
             return
         self.tools = tools
-        langchain_tools = [self.mine_to_lang_parser.parse_tool(tool) for tool in self.tools.values()]
+        langchain_tools: list[BaseTool] = [self.mine_to_lang_parser.parse_tool(tool) for tool in self.tools.values()]
         self.tool_llm = self.llm.bind_tools(langchain_tools)
+
+    def add_tools(self, tools: dict[str, Tool]) -> None:
+        tools |= self.tools
+        self.bind_tools(tools)
 
     def complete_chat(self, message_list: list[Message | SystemMessage | UserMessage | AIMessage | ToolMessage]) -> AIMessage:
         langchain_message_list = [self.mine_to_lang_parser.parse_message(message) for message in message_list]
-        langchain_ai_response: LangchainAIMessage = self.tool_llm.invoke(
-            langchain_message_list) if self.tool_llm else self.llm.invoke(langchain_message_list)
+        langchain_ai_response: LangchainAIMessage = (self.tool_llm or self.llm).invoke(langchain_message_list)
         return self.lang_to_mine_parser.parse_ai_message(langchain_ai_response)
